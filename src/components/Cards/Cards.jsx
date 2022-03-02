82581e5f01d1965fa55e28038183af96a86ac946
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-03-02 09:31:12 -0300

    first commit

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import reactImage from '../../assets/react.png'
+import './styles.css'
+
+function Cards(){
+return (
+    <div className="card">
+        <img src={reactImage} alt="" />
+    </div>
+)
+}
+
+export default Cards;
